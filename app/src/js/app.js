43ac1b0d1041dca38bc7266c5e4800a3efd00774
Nick Cramaro <nick@soughtafter.ca>
2018-03-11 22:43:57 -0400

    Added basic react app

---
+++ app/src/js/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Home from 'Page/home';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }, {
+      test: /\.html$/,
+      use: {
+        loader: 'html-loader'
+      }
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
+      use: {
+        loader: 'url-loader'
+      }
+    }]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    alias: {
+      Page: path.resolve(__dirname, 'src/js/components/page'),
+      Shared: path.resolve(__dirname, 'src/js/components/shared')
+    }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+};
